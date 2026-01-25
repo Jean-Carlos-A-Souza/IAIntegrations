f60982912dcdb9dcb0d2179d070f947bd0f944db
jean-carlos-a-souza <jeansouza1000@gmail.com>
2026-01-25 10:33:06 -0300

    ajuste no banco

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', fn () => redirect('/docs/api'));
 
+
+
+Route::get('/up/db', function () {
+    return [
+        'host' => config('database.connections.pgsql.host'),
+        'db'   => DB::connection()->getDatabaseName(),
+        'ping' => DB::selectOne('select 1 as ok')->ok,
+    ];
+});
